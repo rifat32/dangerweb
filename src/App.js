fd11dc70b82fa7e4ffbc084c1b9fdf18420bdd05
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-03-17 13:10:57 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    baseUrl:'http://127.0.0.1:8000/api/',
+    test:''
+  }
+componentDidMount(){
+  axios.post(`${this.state.baseUrl}test`,{
+   name:'This is test api'
+  })
+  .then((response) => {
+    const test = response.data.test
+    this.setState(() => {
+      return {
+        test:test
+      }
+    })
+
+  })
 }
 
-export default App;
+  render() {
+    return (
+      <div>
+        {this.state.test}
+      </div>
+    )
+  }
+}
+
