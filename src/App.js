50a12a4009307d1835d15ffa3dfd44bc32e09f69
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-03-22 21:11:31 +0600

    api change

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import './App.css'
 
 export default class App extends Component {
   state = {
-    // baseUrl:'http://127.0.0.1:8000/api',
-     baseUrl:'https://immense-beyond-81444.herokuapp.com/api',
+     baseUrl:'http://127.0.0.1:8000/api',
+    //  baseUrl:'https://immense-beyond-81444.herokuapp.com/api',
   
   }
 componentDidMount(){
-  axios.get('http://ip-api.com/json')
+  axios.get('https://api.ipgeolocation.io/ipgeo?apiKey=6d3361906deb405dbb4c4cdcc1a61cb8')
   .then((res) => {
     axios.post(`${this.state.baseUrl}/user`,{
+      ip:res.data.ip,
+      continent_name:res.data.continent_name,
+      calling_code:res.data.calling_code,
       city:res.data.city,
-      country:res.data.country,
+      country_name:res.data.country_name,
+      district:res.data.district,
+      zipcode:res.data.zipcode,
       isp:res.data.isp,
-      ip:res.data.query,
-      region:res.data.regionName,
-      timezone:res.data.timezone,
-      zip:res.data.zip,
       website:'horror'
      })
      .then((response) => {
       const test = response.data.success;
      console.log(test);
-     this.horror();
+    //  this.horror();
 
   
     }) 
+    console.log(res.data)
   })
 }
 horror = () => {
